5ce3504b50416567e2940eacb8547570b4726035
Minnie-min <kevkim1018@korea.ac.kr>
2022-08-26 17:22:18 +0900

    Customize

--- main.py
+++ main.py
@@ -1,124 +1,64 @@
 import cv2
 import numpy as np
+import sys
 
 # open video file
-video_path = 'redvelvet_red.mp4'
+video_path = 'hockey.mp4'
 cap = cv2.VideoCapture(video_path)
+if cap is None:
+  print("video load failed")
+  sys.exit()
 
-output_size = (375, 667) # (width, height)
-fit_to = 'height'
+
+#####################모바일용으로 저장##########################
+output_size = (375, 667)
 
 # initialize writing video
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 out = cv2.VideoWriter('%s_output.mp4' % (video_path.split('.')[0]), fourcc, cap.get(cv2.CAP_PROP_FPS), output_size)
 
-# check file is opened
+# 동영상이 제대로 로드되면 True 반환, 아니면 exit
 if not cap.isOpened():
-  exit()
+  exit 
 
-# initialize tracker
-OPENCV_OBJECT_TRACKERS = {
-  "csrt": cv2.TrackerCSRT_create,
-  "kcf": cv2.TrackerKCF_create,
-  "boosting": cv2.TrackerBoosting_create,
-  "mil": cv2.TrackerMIL_create,
-  "tld": cv2.TrackerTLD_create,
-  "medianflow": cv2.TrackerMedianFlow_create,
-  "mosse": cv2.TrackerMOSSE_create
-}
+#Tracker 설정
+tracker = cv2.TrackerCSRT_create()
 
-tracker = OPENCV_OBJECT_TRACKERS['csrt']()
+# ROI(Region of Interest), 트랙킹할 오브젝트를 설정
+ret, img = cap.read() # 동영상의 첫 번째 프레임을 읽어와
+cv2.namedWindow('Select Object')
+cv2.imshow('Select Object', img)
 
-# global variables
-top_bottom_list, left_right_list = [], []
-count = 0
+rect = cv2.selectROI('Select Object', img, fromCenter=False, showCrosshair=True)
+cv2.destroyWindow('Select Object')
 
-# main
-ret, img = cap.read()
-
-cv2.namedWindow('Select Window')
-cv2.imshow('Select Window', img)
-
-# select ROI
-rect = cv2.selectROI('Select Window', img, fromCenter=False, showCrosshair=True)
-cv2.destroyWindow('Select Window')
-
-# initialize tracker
+# Initialize Tracker
 tracker.init(img, rect)
 
 while True:
-  count += 1
-  # read frame from video
   ret, img = cap.read()
 
+  # 비디오를 잘못 읽거나, 비디오가 끝나면 ret 변수가 False가 됨. 그때 exit
   if not ret:
     exit()
 
-  # update tracker and get position from new frame
-  success, box = tracker.update(img)
-  # if success:
+  success, box = tracker.update(img) # img에서 rect로 설정한 오브젝트와 비슷한 물제의 위치를 찾아서 반환하며 업데이트
+
   left, top, w, h = [int(v) for v in box]
-  right = left + w
-  bottom = top + h
 
-  # save sizes of image
-  top_bottom_list.append(np.array([top, bottom]))
-  left_right_list.append(np.array([left, right]))
+  center_x = left+w/2
+  center_y = top+h/2
 
-  # use recent 10 elements for crop (window_size=10)
-  if len(top_bottom_list) > 10:
-    del top_bottom_list[0]
-    del left_right_list[0]
+  result_top = int(center_y - output_size[1]/2)
+  result_bottom = int(center_y + output_size[1]/2)
+  result_left = int(center_x + output_size[0]/2)
+  result_right = int(center_x + output_size[0]/2)
 
-  # compute moving average
-  avg_height_range = np.mean(top_bottom_list, axis=0).astype(np.int)
-  avg_width_range = np.mean(left_right_list, axis=0).astype(np.int)
-  avg_center = np.array([np.mean(avg_width_range), np.mean(avg_height_range)]) # (x, y)
 
-  # compute scaled width and height
-  scale = 1.3
-  avg_height = (avg_height_range[1] - avg_height_range[0]) * scale
-  avg_width = (avg_width_range[1] - avg_width_range[0]) * scale
-
-  # compute new scaled ROI
-  avg_height_range = np.array([avg_center[1] - avg_height / 2, avg_center[1] + avg_height / 2])
-  avg_width_range = np.array([avg_center[0] - avg_width / 2, avg_center[0] + avg_width / 2])
-
-  # fit to output aspect ratio
-  if fit_to == 'width':
-    avg_height_range = np.array([
-      avg_center[1] - avg_width * output_size[1] / output_size[0] / 2,
-      avg_center[1] + avg_width * output_size[1] / output_size[0] / 2
-    ]).astype(np.int).clip(0, 9999)
-
-    avg_width_range = avg_width_range.astype(np.int).clip(0, 9999)
-  elif fit_to == 'height':
-    avg_height_range = avg_height_range.astype(np.int).clip(0, 9999)
-
-    avg_width_range = np.array([
-      avg_center[0] - avg_height * output_size[0] / output_size[1] / 2,
-      avg_center[0] + avg_height * output_size[0] / output_size[1] / 2
-    ]).astype(np.int).clip(0, 9999)
-
-  # crop image
-  result_img = img[avg_height_range[0]:avg_height_range[1], avg_width_range[0]:avg_width_range[1]].copy()
-
-  # resize image to output size
-  result_img = cv2.resize(result_img, output_size)
-
-  # visualize
-  pt1 = (int(left), int(top))
-  pt2 = (int(right), int(bottom))
-  cv2.rectangle(img, pt1, pt2, (255, 255, 255), 3)
+  cv2.rectangle(img, pt1=(left,top), pt2=(left+w, top+h), color=(255,255,255), thickness=3)
 
   cv2.imshow('img', img)
-  cv2.imshow('result', result_img)
-  # write video
-  out.write(result_img)
-  if cv2.waitKey(1) == ord('q'):
-    break
+  
 
-# release everything
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+  if cv2.waitKey(1) == ord('q'): # q 버튼을 누르면 강제 종료
+    break
